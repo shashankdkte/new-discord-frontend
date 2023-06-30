cbf2f65f5b8f831e3be7aba7ed4f780e87ba7a40
Shashank Dhakate <shashankdkte@gmail.com>
2023-06-30 16:17:59 +0530

    Configuring Redux store

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  userDetails:null
+}
+
+const reducer = (state=initialState,action) => {
+  switch (action.type)
+  {
+    case "DUMMY":
+      return {
+        ...state
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
