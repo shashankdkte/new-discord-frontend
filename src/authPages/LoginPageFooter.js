00dce9688ab1d8ec3a0a0129b7721b00d9620d4a
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-03 18:25:57 +0530

    Added Tooltip

--- src/authPages/LoginPageFooter.js
+++ src/authPages/LoginPageFooter.js
@@ -2,6 +2,9 @@ import React from 'react'
 import CustomPrimaryButton from '../shared/CustomPrimaryButton'
 import {useNavigate} from "react-router-dom"
 import RedirectInfo from '../shared/RedirectInfo';
+import {
+  Tooltip
+} from '@mui/material';
 const LoginPageFooter = (props) => {
   const { isFormValid, handleLogin } = props;
   const navigate = useNavigate();
@@ -9,21 +12,32 @@ const LoginPageFooter = (props) => {
   const handlePushToRegisterPage = () => {
     navigate("/register")
   }
+  const getFormNotValidMessage = () => {
+  return "Enter correct email address and password";
+};
+const getFormValidMessage = () => {
+  return "Press to Log In";
+};
   return (
+    <>
+      <Tooltip title={!isFormValid ? getFormNotValidMessage():getFormValidMessage()}>
+
     <div>
       <CustomPrimaryButton
         label="Log In"
         additionalStyles={{ marginTop: "30px" }}
         disabled={!isFormValid}
         onClick={handleLogin}
-      />
+        />
+        </div>
+        </Tooltip>
       <RedirectInfo
         text="Need an account? "
         redirectText="Create an account"
         additionalStyles={{ marginTop: "5px" }}
         redirectHandler={handlePushToRegisterPage}
-      ></RedirectInfo>
-    </div>
+        ></RedirectInfo>
+        </>
   )
 }
 
