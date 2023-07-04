d51d3b3070358e0060d268777a3d826170e058ab
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-05 00:43:46 +0530

    Added Logout

--- src/api.js
+++ src/api.js
@@ -1,10 +1,26 @@
 import axios from "axios";
+import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5002/api",
-  timeout: 1000
+  timeout: 1000,
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem("user");
+
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 export const login = async (data) => {
   try {
     
@@ -28,3 +44,12 @@ export const register = async (data) => {
     }
   }
 }
+
+
+const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
+};
