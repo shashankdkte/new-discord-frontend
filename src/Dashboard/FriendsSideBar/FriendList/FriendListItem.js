6ee755517bcb57490b34a6da95f1874f4a72e539
Shashank Dhakate <shashankdkte@gmail.com>
2023-07-07 14:08:27 +0530

    Preparing FriendList

---
+++ src/Dashboard/FriendsSideBar/FriendList/FriendListItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Typography } from '@mui/material'
+import Avatar from '../../../shared/Avatar';
+
+const FriendListItem = (props) => {
+
+  const { id,username,isOnline} = props;
+  return (
+    <Button style={{
+      width: "100%",
+      height: "42px",
+      marginTop: "10px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      textTransform: "none",
+      color: "black",
+      position:"relative"
+    }}>
+      <Avatar username={username}/>
+      <Typography style={{
+        marginLeft: "7px",
+        fontWeight: "700",
+        color:"#8e9297"
+      }} variant='subtitle1' align='left' >{username}</Typography>
+   </Button>
+  )
+}
+
+export default FriendListItem
