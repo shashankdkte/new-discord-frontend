ff63d5388db1fdd5f291fcedf52492cec4650656
Shashank Dhakate <shashankdkte@gmail.com>
2023-12-06 17:04:13 +0530

    Local Video

--- src/Dashboard/Room/VideoContainer.js
+++ src/Dashboard/Room/VideoContainer.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {styled} from "@mui/system"
+import { styled } from "@mui/system";
+import { connect } from "react-redux";
+import Video from "./Video";
 
 
 const MainContainer = styled("div")({
@@ -8,8 +10,14 @@ const MainContainer = styled("div")({
   display: "flex",
   flexWrap:"wrap"
 })
-const VideoContainer = () => {
-  return <MainContainer></MainContainer>
+const VideoContainer = ({localStream}) => {
+  return <MainContainer>
+    <Video stream={localStream} isLocalStream/>
+  </MainContainer>
 }
-
-export default VideoContainer
+const mapStoreStateToProps = ({ room }) => {
+  return {
+    ...room,
+  }
+}
+export default connect(mapStoreStateToProps)(VideoContainer)
